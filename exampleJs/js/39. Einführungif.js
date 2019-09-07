7b41771d5587b25d59e8d7d8a15877c5facfbe4e
Verenasworld <verena.felizidas@gmail.com>
2019-09-07 22:39:12 +0200

    39. Einführung in if und else

---
+++ exampleJs/js/39. Einführungif.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let x = 10;
+// nach if = immer eine Bedingung, diese wird abgefagt ob bedingung erfüllt oder nicht, else = wenn die Bedinung nicht erfüllt ist dann mache das.....
+if (x < 5){
+
+    console.log(" x ist kleiner als 15");
+} else {
+    console.log("Else");
+
+}
+
+console.log("Fertig");
