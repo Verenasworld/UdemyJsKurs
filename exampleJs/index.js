db062df7a2a92e58f287d73f8054c8d08806e797
Verenasworld <verena.felizidas@gmail.com>
2019-09-07 21:44:27 +0200

    36. Mit Arrays arbeiten

--- exampleJs/index.js
+++ exampleJs/index.js
@@ -1,1 +1,14 @@
+"use strict";
 
+let person = ["Verena","Leo", "Karin"];
+person.push("Susanne");
+person.unshift("Andi");
+console.log(person);
+
+person.pop();
+console.log(person);
+console.log(person.length);
+let removedPerson = person.shift();
+console.log(removedPerson);
+console.log(person);
+console.log("in dem Kurs sind " + person.length + " Teilnehmer");
