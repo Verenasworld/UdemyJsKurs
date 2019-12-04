9eadf302b32b75ed207a2aa178de7b34261da8a1
Verenasworld <verena.felizidas@gmail.com>
2019-12-04 10:31:10 +0100

    add Game Loop logic (step1)

--- exampleJs/Spiel/index.js
+++ exampleJs/Spiel/index.js
@@ -1,37 +1,58 @@
 "use strict";
 
 
- //klasse erstellen für spielfläche um diese anzeigen zu können
+                                                                                 //klasse erstellen für spielfläche um diese anzeigen zu können
 
 class Renderer {
 
 
-    //konstruktor bekommt das element übergeben mit dem der renderer arbeiten soll.
+                                                                                //konstruktor bekommt das element übergeben mit dem der renderer arbeiten soll.
     constructor(element){
-        this.element= element;
+        this.element= element;                                                      //  game element in klasse gespeichert
+        this.setup()                                                                  // konstruktor führt die setup methode aus
+
     }
 
-    //    render methode implemendieren!
-        render() {
-                                                                        // console.log(this);
-                                                                        //eine box erstellen
+        setup(){                                                                   //erstellen eines Einstellung Setup zum erstellen stylen & hinzufügen zum DOM einer Box.
+
+
+            // eine box erstellen
             let box = document.createElement("div");
 
-                                                                            //    box positionieren
+            // box positionieren
             box.style.position = "absolute";
             box.style.top = "20px";
-            box.style.top = "20px";
+            box.style.left = "20px";
 
-                                                                             //    einfügen der Box in mein Spielfeld
-            this.element.appendChild(box);
+            // box stylen
+            box.style.width = "20px";
+            box.style.height = "20px";
+            box.style.backgroundColor = "white";
+            box.style.border = "1px solid black";
+
+            this.element.appendChild(box);                                          // einfügen der Box in mein Spielfeld
+            this.box = box;                                                         //    setzte die box auf die box ( dient dazu das ich in render methode auf die box zugreifen kann)
 
         }
+
+
+        render() {                                                                    //    render methode implemendieren!
+
+        this.box.style.top = "40px";
+
+
+    }
 
     
 }
 
-//die verbindung zu Html DOM ?
-let renderer = new Renderer(document.getElementById("game"));
 
-//ausführung der methode render
-renderer.render();
+let renderer = new Renderer(document.getElementById("game"));                     //die verbindung zu Html DOM ?
+
+
+setInterval(() => {                                                             // eine funktion erstellen die mir die render methode 1 mal pro sekunde ausführt
+
+    renderer.render();                                                                    //ausführung der methode render ( 1x pro sekunde)
+},1000);
+
+
