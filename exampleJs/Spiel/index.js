3cd76634e9997c803688892cc881886d98d1f1c0
Verenasworld <verena.felizidas@gmail.com>
2019-12-04 15:19:09 +0100

    add counter

--- exampleJs/Spiel/index.js
+++ exampleJs/Spiel/index.js
@@ -6,53 +6,51 @@
 class Renderer {
 
 
-                                                                                //konstruktor bekommt das element übergeben mit dem der renderer arbeiten soll.
-    constructor(element){
-        this.element= element;                                                      //  game element in klasse gespeichert
+    //konstruktor bekommt das element übergeben mit dem der renderer arbeiten soll.
+    constructor(element) {
+        this.element = element;                                                      //  game element in klasse gespeichert
         this.setup()                                                                  // konstruktor führt die setup methode aus
 
     }
 
-        setup(){                                                                   //erstellen eines Einstellung Setup zum erstellen stylen & hinzufügen zum DOM einer Box.
+    setup() {                                                                   //erstellen eines Einstellung Setup zum erstellen stylen & hinzufügen zum DOM einer Box.
 
 
-            // eine box erstellen
-            let box = document.createElement("div");
+        // eine box erstellen
+        let box = document.createElement("div");
 
-            // box positionieren
-            box.style.position = "absolute";
-            box.style.top = "20px";
-            box.style.left = "20px";
+        // box positionieren
+        box.style.top = "20px";
+        box.style.left = "20px";
+        box.style.position = "absolute";
 
-            // box stylen
-            box.style.width = "20px";
-            box.style.height = "20px";
-            box.style.backgroundColor = "white";
-            box.style.border = "1px solid black";
+        // box stylen
+        box.style.width = "20px";
+        box.style.height = "20px";
+        box.style.backgroundColor = "white";
+        box.style.border = "1px solid black";
 
-            this.element.appendChild(box);                                          // einfügen der Box in mein Spielfeld
-            this.box = box;                                                         //    setzte die box auf die box ( dient dazu das ich in render methode auf die box zugreifen kann)
-
-        }
-
-
-        render() {                                                                    //    render methode implemendieren!
-
-        this.box.style.top = "40px";
-
+        this.element.appendChild(box);                                          // einfügen der Box in mein Spielfeld
+        this.box = box;                                                         //    setzte die box auf die box ( dient dazu das ich in render methode auf die box zugreifen kann)
 
     }
 
-    
+
+    render(position) {                                                                    //    render methode implemendieren!
+
+        this.box.style.top = position + "40px";
+    }
+
 }
 
 
 let renderer = new Renderer(document.getElementById("game"));                     //die verbindung zu Html DOM ?
-
+let counter = 0;
 
 setInterval(() => {                                                             // eine funktion erstellen die mir die render methode 1 mal pro sekunde ausführt
-
-    renderer.render();                                                                    //ausführung der methode render ( 1x pro sekunde)
+    counter = counter + 1;
+    renderer.render(counter);                                                                    //ausführung der methode render ( 1x pro sekunde)
 },1000);
 
 
+//TODO counter position korr. the steps are to big
