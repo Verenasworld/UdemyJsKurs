43145c5a93e17c2c721246913e4cd7b2573f7a96
Verenasworld <verena.felizidas@gmail.com>
2019-12-05 13:28:52 +0100

    first add function for the control elements

--- exampleJs/Spiel/index.js
+++ exampleJs/Spiel/index.js
@@ -3,11 +3,9 @@
 
                                                                                  //klasse erstellen für spielfläche um diese anzeigen zu können
                                                                                 // renderer = bezeichnet den Prozess das wir irgendwas anzeigen
-class Renderer {
+    class Renderer {                                                              //konstruktor bekommt das element übergeben mit dem der renderer arbeiten soll.
 
-
-    //konstruktor bekommt das element übergeben mit dem der renderer arbeiten soll.
-    constructor(element) {
+    constructor(element) {                                                          //  constructor funktion ???
         this.element = element;                                                      //  game element in klasse gespeichert
         this.setup()                                                                  // konstruktor führt die setup methode aus
 
@@ -15,41 +13,79 @@ class Renderer {
 
     setup() {                                                                   //erstellen eines Einstellung Setup zum erstellen stylen & hinzufügen zum DOM einer Box.
 
-
-        // eine box erstellen
+                                                                                // eine box erstellen
         let box = document.createElement("div");
-        // box positionieren
+                                                                                // box positionieren
         box.style.position = "absolute";
         box.style.top = "20px";
         box.style.left = "20px";
-
-
-        // box stylen
+                                                                                    // box stylen
         box.style.width = "20px";
         box.style.height = "20px";
         box.style.backgroundColor = "white";
 
         this.element.appendChild(box);                                          // einfügen der Box in mein Spielfeld
-        this.box = box;                                                         //    setzte die box auf die box ( dient dazu das ich in render methode auf die box zugreifen kann)
-
+        this.box = box;                                                         // setzte die box auf die box ( dient dazu das ich in render methode auf die box zugreifen kann)
     }
 
 
-    render(position) {                                                                    //    render methode implemendieren!
+    render(position) {                                                            //    render methode implemendieren!
 
-        this.box.style.top = position + "5px";
+        this.box.style.top = position + "px";
+    }
+}
+
+
+
+
+class Box {                                                                           //klasse erstellen für die "steuerung" der box
+    constructor(){
+        this.position = 0;
+
+    }
+    runLoop(){                                                                                 //erstekken einer methode _
+       this.position ++;                                                     //verschieben der box um eines nach unten!!!
+    };
+
+    moveUp(){
+        this.position = this.position -20;                                       //wenn box geklickt wird dann ändere position 20 px nach oben
+
+    }
+}
+
+
+class Game{                                                                             // klasse erstellen  - die das spiel "representieren" soll
+    constructor(element){                                                                //die klasse hat einen  constructor dem ich das element übergebe
+            this.renderer = new Renderer(element);
+            this.box = new Box();
+            this.element = element;
+            this.setup();
+
+    }
+
+    setup(){
+        this.element.addEventListener("click", () => {                                //wenn mein spielfeld geklickt wird dann soll etwas passieren -
+            this.box.moveUp();                                                    //diese methode soll ausgedführt werden
+            }, false);
+
+    }
+    start(){
+        setInterval(() => {                                                             // eine funktion erstellen die mir die render methode 1 mal pro sekunde ausführt
+            this.box.runLoop();                                                                  //steuerung ??
+            this.renderer.render(this.box.position);                                            //ausführung der methode render
+        },100);
     }
 
 }
 
 
-let renderer = new Renderer(document.getElementById("game"));                     //die verbindung zu Html DOM ?
-let counter = 0;
+let game = new Game(document.getElementById("game"));                                               //die verbindung zu Html DOM ? erstellung eines objektes ?! hier übergebe ich mein spielfeld element
+game.start();
 
-setInterval(() => {                                                             // eine funktion erstellen die mir die render methode 1 mal pro sekunde ausführt
-    counter = counter + 1;
-    renderer.render(counter);                                                                    //ausführung der methode render ( 1x pro sekunde)
-},100);
+
+
 
 
 //TODO counter position korr. the steps are to big         done !!!
+
+// TODO change in cor. pos. ( relative & absolute dont working )
