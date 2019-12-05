374fb06c58d4e5c1ab3025d01fe79faeb59f8cd4
Verenasworld <verena.felizidas@gmail.com>
2019-12-05 11:03:14 +0100

    done todo

--- exampleJs/Spiel/index.js
+++ exampleJs/Spiel/index.js
@@ -2,7 +2,7 @@
 
 
                                                                                  //klasse erstellen für spielfläche um diese anzeigen zu können
-
+                                                                                // renderer = bezeichnet den Prozess das wir irgendwas anzeigen
 class Renderer {
 
 
@@ -18,17 +18,16 @@ class Renderer {
 
         // eine box erstellen
         let box = document.createElement("div");
-
         // box positionieren
+        box.style.position = "absolute";
         box.style.top = "20px";
         box.style.left = "20px";
-        box.style.position = "absolute";
+
 
         // box stylen
         box.style.width = "20px";
         box.style.height = "20px";
         box.style.backgroundColor = "white";
-        box.style.border = "1px solid black";
 
         this.element.appendChild(box);                                          // einfügen der Box in mein Spielfeld
         this.box = box;                                                         //    setzte die box auf die box ( dient dazu das ich in render methode auf die box zugreifen kann)
@@ -38,7 +37,7 @@ class Renderer {
 
     render(position) {                                                                    //    render methode implemendieren!
 
-        this.box.style.top = position + "40px";
+        this.box.style.top = position + "5px";
     }
 
 }
@@ -50,7 +49,7 @@ let counter = 0;
 setInterval(() => {                                                             // eine funktion erstellen die mir die render methode 1 mal pro sekunde ausführt
     counter = counter + 1;
     renderer.render(counter);                                                                    //ausführung der methode render ( 1x pro sekunde)
-},1000);
+},100);
 
 
-//TODO counter position korr. the steps are to big
+//TODO counter position korr. the steps are to big         done !!!
