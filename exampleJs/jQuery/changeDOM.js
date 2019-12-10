d048cee9470d06ca15ce28f7f39f1c04444c85b6
Verenasworld <verena.felizidas@gmail.com>
2019-12-10 11:46:42 +0100

    Method prepend- adds the new  li -element  at the beginning.

--- exampleJs/jQuery/changeDOM.js
+++ exampleJs/jQuery/changeDOM.js
@@ -5,7 +5,7 @@
 jQuery(document).ready(function ($){
 $(".test").click(function () {
     let li = $.parseHTML("<li>Team</li>");
-    $(".test").append(li);
-
+    let li1 = $.parseHTML("<li>Shop</li>");
+    $(".test").append(li).prepend(li1);
 });
 });
