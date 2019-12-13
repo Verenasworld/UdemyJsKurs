c8db32b4e202e089f8281b873db5ac1e98da98cb
Verenasworld <verena.felizidas@gmail.com>
2019-12-13 16:45:35 +0100

    add remove for closing on keydown - Escape

--- exampleJs/jQuery/Lightbox/lightbox.js
+++ exampleJs/jQuery/Lightbox/lightbox.js
@@ -1,28 +1,26 @@
 "use strict";
 
 jQuery(document).ready(function ($) {
+    $(document).keydown(function (event) {
+     if (event.key ==="Escape"){
+         $("#overlay").remove();
+         $("#overlay-container").remove();
+     }
+    });
+
     $(".gallery-item").click(function (event) {
         event.preventDefault();
-    //    bedeutet wenn ich auf Bild drücke dann passiert jetzt nichts den wir wollem mit js die funktionalität überschreiben!
-
-    //    div in DOM einfügen für Overlay Element
         let overlay = $.parseHTML("<div id='overlay'></div>");
         $("body").append(overlay);
 
         let image = $.parseHTML("<img id='image-overlay'>");
         let src = $(this).attr("href");
         $(image).attr("src", src);
-        $(image).click(function () {
-                $("#overlay").remove();
-                $("#overlay-container").remove();
-
-        });
 
         let container = $.parseHTML("<div id='overlay-container'></div>");
         $(image).click(function () {
             $("#overlay").remove();
             $("#overlay-container").remove();
-
         });
         $(container).append(image);
         $("body").append(container);
