30e9b1a77aed9945a15a9f3ce341da86148a93a7
Verenasworld <verena.felizidas@gmail.com>
2019-12-13 12:41:17 +0100

    add new branch for design a Lightbox Gallery

--- exampleJs/jQuery/changeDOM.js
+++ exampleJs/jQuery/changeDOM.js
@@ -1,11 +1,14 @@
 "use strict";
 
 
-
+//tells the HTML DOM to wait until it is ready to load Html (for example, if script is linked in the head)
 jQuery(document).ready(function ($){
 $(".test").click(function () {
-    let li = $.parseHTML("<li>Team</li>");
-    let li1 = $.parseHTML("<li>Shop</li>");
-    $(".test").append(li).prepend(li1);
+    let li = $.parseHTML("<li>Team ( setzt sich ans Ende des li Elements mit append) </li>");
+    let li1 = $.parseHTML("<li>Shop ( setzt sich an den Anfang des li Elements mit prebend)</li>");
+    let libefore = $.parseHTML("<li>Before (vor dem Li element) </li>");
+    let liafter = $.parseHTML("<li>After ( nach dem  li element) </li>");
+    $(".test").append(li).prepend(li1).before(libefore).after(liafter);
+
 });
 });
