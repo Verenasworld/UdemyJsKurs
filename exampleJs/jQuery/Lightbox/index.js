6c73bbae677b0b47b716752121d3960b4671a3ba
Verenasworld <verena.felizidas@gmail.com>
2019-12-13 15:32:00 +0100

    position design with css

--- exampleJs/jQuery/Lightbox/index.js
+++ exampleJs/jQuery/Lightbox/index.js
@@ -6,8 +6,13 @@ jQuery(document).ready(function ($) {
     //    bedeutet wenn ich auf Bild drücke dann passiert jetzt nichts den wir wollem mit js die funktionalität überschreiben!
 
     //    div in DOM einfügen für Overlay Element
-        let overlay = $.parseHTML("<div id=' overlay'></div>");
-        $("body").append(overlay)
-    })
+        let overlay = $.parseHTML("<div id='overlay'></div>");
+        $("body").append(overlay);
+
+        let image = $.parseHTML("<img> id='image-overlay'</img>");
+        let src = $(this).attr("href");
+        $(image).attr("src", src);
+        $("body").append(image);
+    });
     
 });
